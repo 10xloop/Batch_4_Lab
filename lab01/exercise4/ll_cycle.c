5e87977026a6ef83cb60c425f0c25cd868d92b3f
hacker <zeshan@10x.com>
2023-03-20 10:45:22 +0500

    lab01 ex_4 Floyd's tortoise and hare algorithm followed

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,24 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    
+node* fast;
+node* slow;
+
+fast=head;
+slow=head;
+
+while (fast!=NULL && fast->next!=NULL)
+{
+   slow=slow->next;       // advance by one node
+   fast=fast->next->next;     // advance by 2 nodes
+
+    if(slow==fast)
+    {
+        return 1;
+    }
+
+}
+
+return 0;
 }
