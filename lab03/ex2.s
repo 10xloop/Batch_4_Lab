2a79ae1b1fd27e1675552c361a174073730156a7
hacker <zeshan@10x.com>
2023-03-27 23:07:18 +0500

    ex2.s Comments added to compare the .s code with .c and for debugging

--- lab03/ex2.s
+++ lab03/ex2.s
@@ -57,7 +57,7 @@ loop:
     add t2, x0, a0      #return value from fun
     add t3, s2, s3
     sw t2, 0(t3)      #store @ destination array
-    add s0, s0, t2
+    add s0, s0, t2     #Sum=Sum+Current Val
     addi t0, t0, 1
     jal x0, loop
 exit:
