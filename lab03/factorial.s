fb2dadeb5891010873ad57ab512aa952e8894e15
hacker <zeshan@10x.com>
2023-03-27 23:08:17 +0500

    factorial label defined

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -5,12 +5,13 @@ n: .word 8
 
 .text
 main:
+    li t1,1
     la t0, n
     lw a0, 0(t0)
-    jal ra, factorial
+    jal factorial
 
-    addi a1, a0, 0
-    addi a0, x0, 1
+    addi a1, a0, 0    #return value from function
+    addi a0, x0, 1      #To print integer on console
     ecall # Print Result
 
     addi a1, x0, '\n'
@@ -22,3 +23,14 @@ main:
 
 factorial:
     # YOUR CODE HERE
+   mul t1,t1,a0    #t1=t1*a0
+   addi a0,a0,-1   #a0=a0-1
+   beq a0, x0, jumptomain
+   
+   jal x0,factorial
+   
+   
+   jumptomain:
+   mv a0,t1
+   jr ra
+   
